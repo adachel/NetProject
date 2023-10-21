e61e30b70033ba422833be5336f3e242f33b8a28
Dmitry <adachel74@gmail.com>
2023-10-21 10:47:26 +0500

    final

--- Socket/client.py
+++ Socket/client.py
@@ -3,6 +3,7 @@ import threading
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
+print("дополнительно - для отпрaвления привaтного сообщения введите: pr_кому~ сообщение")
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # откр сокет под именем client
@@ -17,18 +18,15 @@ def receive():
             message = client.recv(1024).decode('ascii')
             arr = message.split('~')
             # print(arr)
-            try:
-                # print(f'pr_{nickname}')
-                # print(arr[0])
+            # print(arr[0])
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            elif len(arr) > 1:
                 temp = arr[0].split(" ")
-                # print(temp[1])
                 if temp[1] == f'pr_{nickname}':
-                    print('privat: ' + message)
-            except:
-                if message == 'NICK':
-                    client.send(nickname.encode('ascii'))
-                else:
-                    print(message)
+                    print("privat: " + temp[0] + " " + arr[1])
+            else:
+                print(message)
         except:
             # Close Connection When Error
             print("An error occured!")
@@ -38,6 +36,13 @@ def receive():
 def write():
     while True:
         message = '{}: {}'.format(nickname, input(''))
+        arr = message.split("~")
+        # print(arr)
+        if len(arr) > 1:
+            temp = arr[0].split(" ")
+            mes = temp[1].split("_")
+            print("privat: " + mes[1] + ":" + " " + arr[1])
+
         client.send(message.encode('ascii'))
 
 # Starting Threads For Listening And Writing
